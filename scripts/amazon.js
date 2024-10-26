43e181fe45d9d63a38182811877e3081c2d63a1b
Vinnykells <kellydwaynez@gmail.com>
2024-10-26 10:24:33 +0100

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,12 +1,16 @@
-import {cart, addToCart} from '../data/cart.js';
-import { products } from "../data/products.js";
+import { cart, addToCart } from "../data/cart.js";
+import { products, loadProducts } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
-let productsHtml = "";
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHtml =
-    productsHtml +
-    `
+function renderProductsGrid() {
+  let productsHtml = "";
+
+  products.forEach((product) => {
+    productsHtml =
+      productsHtml +
+      `
   <div class="product-container">
   <div class="product-image-container">
     <img class="product-image"
@@ -59,26 +63,27 @@ products.forEach((product) => {
   </button>
 </div>
   `;
-});
-
-document.querySelector(".js-products-grid").innerHTML = productsHtml;
-
-function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
   });
 
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  document.querySelector(".js-products-grid").innerHTML = productsHtml;
+
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  }
+
+  updateCartQuantity();
+
+  document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.dataset.productId;
+      cart.addToCart(productId);
+      updateCartQuantity();
+    });
+  });
 }
-
-updateCartQuantity();
-
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
-    cart.addToCart(productId);
-    updateCartQuantity();
-  });
-});
